7f9e52c73f38c8c247edc1f3c67e1666198574dc
Kauã Dias de Assis <007940@libertas.edu.br>
2026-05-11 19:37:16 -0300

    Delete atividades directory

--- atividades/produtos.py
+++
@@ -1,8 +0,0 @@
-# Leia valor
-valor = float(input("Digite o valor: "))
-# Leia Quantidade
-qtde = float(input("Digite a quantidade: "))
-# Total <- Valor * Quantidade 
-total = valor * qtde
-# Escreva Total
-print("O valor é ", total)
