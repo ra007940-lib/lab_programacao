4f456af868f15b27ff037466b26f3b317872a751
ra007940-lib <007940@libertas.edu.br>
2026-05-11 19:25:50 -0300

    Add files via upload

---
+++ atividades/produtos.py
@@ -0,0 +1,8 @@
+# Leia valor
+valor = float(input("Digite o valor: "))
+# Leia Quantidade
+qtde = float(input("Digite a quantidade: "))
+# Total <- Valor * Quantidade 
+total = valor * qtde
+# Escreva Total
+print("O valor é ", total)
